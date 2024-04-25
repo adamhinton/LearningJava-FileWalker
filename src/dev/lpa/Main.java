00c2ee4fc806aa41bdaa327b055e211e96ed350d
adamd <adam.d.hinton@gmail.com>
2024-04-25 10:47:26 -0400

    better showing of file paths

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -24,14 +24,36 @@ public class Main {
 
     private static class StatsVisitor extends SimpleFileVisitor<Path>{
 
+        // implicitly initialized to 0
+        private int level;
+
         @Override
         public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
             Objects.requireNonNull(file);
             Objects.requireNonNull(attrs);
-            System.out.println(file.getFileName());
+            System.out.println("\t".repeat(level) + file.getFileName());
             return FileVisitResult.CONTINUE;
         }
 
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
 
+            Objects.requireNonNull(dir);
+            Objects.requireNonNull(attrs);
+
+            // inc when visiting dir
+            level++;
+            System.out.println("\t".repeat(level) + dir.getFileName());
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+            Objects.requireNonNull(dir);
+//            if (exc != null)
+//                throw exc;
+            level--;
+            return FileVisitResult.CONTINUE;
+        }
     }
 }
