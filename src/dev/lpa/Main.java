32ba4566d38bbaf3a06a51adfcbe0da70ad5a790
adamd <adam.d.hinton@gmail.com>
2024-04-25 10:42:34 -0400

    override FileVisitResult

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,15 +1,37 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package dev.lpa;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Depth first
+
+        Path startingPath = Path.of(".");
+        FileVisitor<Path> statsVisitor = new StatsVisitor();
+
+        try{
+            Files.walkFileTree(startingPath, statsVisitor);
         }
+        catch(IOException e){
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private static class StatsVisitor extends SimpleFileVisitor<Path>{
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            Objects.requireNonNull(file);
+            Objects.requireNonNull(attrs);
+            System.out.println(file.getFileName());
+            return FileVisitResult.CONTINUE;
+        }
+
+
     }
 }
